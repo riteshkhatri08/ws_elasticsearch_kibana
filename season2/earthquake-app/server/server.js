5a9fe7651b4aef58c9ff875c489e369b5fb975d9
riteshkhatri08 <riteshkhatri1512@gmail.com>
2024-03-16 22:17:25 +0530

    Setup a basic node.js express app

---
+++ season2/earthquake-app/server/server.js
@@ -0,0 +1,16 @@
+console.log("EARTHQUAKE-APP is starting.....");
+
+// get express module
+const express = require("express");
+
+// get express app instance from express module
+const app = express();
+
+// set port number
+const port = 3001;
+
+// start the app server by listening on above port
+app.listen(port, () => {
+  console.log("EARTHQUAKE-APP is running.....");
+  console.log(`Server listening at http://localhost:${port}`);
+});
